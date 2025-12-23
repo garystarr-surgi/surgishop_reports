fa366eb196d190a317100e99b4574449b43af991
garystarr-surgi <gary.starr@surgishop.com>
2025-12-23 13:15:45 -0500

    Add files via upload

---
+++ surgishop_reports/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Surgishop Reports",
+            "color": "blue",
+            "icon": "octicon octicon-graph",
+            "type": "module",
+            "label": _("Surgishop Reports")
+        }
+    ]
